07a3fecf744bb8ec8430304de5281ef32e5dabb8
Arne Reijntjes <arne123@live.nl>
2020-09-15 13:04:19 +0200

    Initial commit

---
+++ build/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var toggleMenu = function() {
+  document.getElementById('menu').classList.toggle('menu-active');
+
+  // Toggle de afbeelding tussen hamburger menu en kruisje etc.
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        purple: '#4f4082',
+        purpleLight: '#7c7fbb',
+        purpleAccent: '#3d3663',
+        pink: '#f6b2b6'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
